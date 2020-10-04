e55dc33124a8252174b58c301269cf9a4800e9f0
Shawn Deggans <shawndeggans@gmail.com>
2020-10-04 23:14:14 +0000

    First check in of SQLCLR Example

---
+++ UpperCaseFunction/UpperCaseFunc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpperCaseFunction
+{
+    public static class UpperCaseFunc
+    {
+        public static string MakeUpperCase(string TargetText) { return TargetText.ToUpper(); }
+    }
+}
